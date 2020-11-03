5e268359b8aaedd3ba2d2da9c984f97a47e8d565
JasterRogue <ryanmadigan141@gmail.com>
2020-11-03 20:39:32 +0000

    - Two animations with mask kinda work - Added hat and sword to models
    Signed-off-by: JasterRogue <ryanmadigan141@gmail.com>

---
+++ Animation/Animation GM/Assets/Scripts/KnightControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightControl : MonoBehaviour
+{
+    Animator knightAnimator;
+
+    // Start is called before the first frame update
+
+    float speed = 0.0f;
+    private float acceleration = 0.006f;
+
+    void Start()
+    {
+        knightAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        knightAnimator.SetFloat("Blend", speed);
+
+        //print("Speed: " + speed);
+        speed = Mathf.Clamp(speed, 0, 1);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed += acceleration;
+        }
+
+        else
+        {
+            knightAnimator.SetBool("isRunning", false);
+            knightAnimator.SetBool("isIdle", true);
+
+            speed -= acceleration;
+        }
+
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            knightAnimator.SetBool("isRunningBackwards", true);
+            knightAnimator.SetBool("isIdle", false);
+            knightAnimator.SetBool("isRunning", false);
+        }
+
+        else
+        {
+            knightAnimator.SetBool("isRunningBackwards", false);
+            knightAnimator.SetBool("isIdle", true);
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            knightAnimator.SetBool("isWaving", true);
+        }
+
+        else
+        {
+            knightAnimator.SetBool("isWaving", false);
+        }
+
+
+    }
+}
