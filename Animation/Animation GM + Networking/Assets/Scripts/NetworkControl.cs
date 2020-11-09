d60b4fc3ed6dc81de3f46c78d4a01a12d57cc756
JasterRogue <ryanmadigan141@gmail.com>
2020-11-09 19:54:48 +0000

    - Started working on networking
    Signed-off-by: JasterRogue <ryanmadigan141@gmail.com>

---
+++ Animation/Animation GM + Networking/Assets/Scripts/NetworkControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkControl : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Connect to master server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //base.OnConnectedToMaster();
+        Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server!");
+    }
+}
