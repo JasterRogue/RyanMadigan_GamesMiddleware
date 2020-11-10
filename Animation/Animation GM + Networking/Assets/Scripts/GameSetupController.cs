ace21677859ab870e81e1e2ee872a799229cd97c
JasterRogue <ryanmadigan141@gmail.com>
2020-11-10 16:52:20 +0000

    - Networking kind of working
    Signed-off-by: JasterRogue <ryanmadigan141@gmail.com>

---
+++ Animation/Animation GM + Networking/Assets/Scripts/GameSetupController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class GameSetupController : MonoBehaviour
+{
+    void Start()
+    {
+        CreatePlayer(); 
+    }
+
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "unitychan"), Vector3.zero, Quaternion.identity);
+    }
+}
---
+++ Animation/Animation GM + Networking/Assets/Scripts/QuickStartRoomController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+
+public class QuickStartRoomController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int multiplayerSceneIndex;
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //When you successfully join a room
+        Debug.Log("Joined room");
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        //load into multiplayer scene
+        if(PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting Game");
+            PhotonNetwork.LoadLevel(multiplayerSceneIndex);
+        }
+    }
+
+}
